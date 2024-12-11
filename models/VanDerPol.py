42d895115dfc1a9cc621f16e5f198a5b1fdaf68c
刘珂聿 <134591888+Serenity-LIU@users.noreply.github.com>
2024-12-11 23:56:43 +0800

    Update VanDerPol.py

--- models/VanDerPol.py
+++ models/VanDerPol.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-from torchdiffeq import odeint
 
 class VanDerPol(nn.Module):
     def __init__(self, alpha1, alpha2, W):
@@ -28,7 +27,7 @@ class RNNwithODE(nn.Module):
     def input_to_hidden(self, x):
         h1_initial = torch.randn(x.size(0), self.hidden_dim)
         h2_initial = torch.randn(x.size(0), self.hidden_dim)
-        hidden = torch.stack([h1_initial, h2_initial])
+        hidden = torch.stack([h1_initial, h2_initial]).to(x.device)
         return hidden
     
     def forward(self, x, hidden = None, t_span = None):
@@ -38,6 +37,8 @@ class RNNwithODE(nn.Module):
         if t_span is None:
             t_span = torch.linspace(0, 1, 5, device=hidden.device)
 
-        hidden = odeint(self.ode_func, hidden, t_span)
-        output = self.fc(hidden[-1, 0])
+        # hidden = odeint(self.ode_func, hidden, t_span)
+        for _ in range(len(t_span)):
+            hidden += 0.2 * self.ode_func(t_span, hidden)
+        output = self.fc(hidden[0])
         return output, hidden
